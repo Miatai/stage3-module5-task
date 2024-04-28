ea39ff05e4363035d4e2e237fce40d19e4c7f9a5
Alexander Skorb <nullus25@gmail.com>
2024-04-28 20:01:27 +0300

    change DB from h2 to postgresql

---
+++ module-repository/src/main/java/com/mjc/school/repository/config/RepositoryConfig.java
@@ -0,0 +1,9 @@
+package com.mjc.school.repository.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class RepositoryConfig {
+}
